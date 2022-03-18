db4e4a7d9591cb254d2fdd64036dd87447551045
Olukoyigrace <101392652+Olukoyigrace@users.noreply.github.com>
2022-03-18 16:49:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const change = document.getElementById("image1");
+
+change.addEventListener("click", (event) =>{
+    event.target.style.background ="blue";
+    event.target.style.color ="white";
+});
+
+const cha = document.getElementById("image2");
+
+cha.addEventListener("click", (event) =>{
+    event.target.style.background ="blue";
+    event.target.style.color ="white";
+});
+
+const chan = document.getElementById("image3");
+
+chan.addEventListener("click", (event) =>{
+    event.target.style.background ="blue";
+    event.target.style.color ="white";
+});
+
+const chang = document.getElementById("image4");
+
+chang.addEventListener("click", (event) =>{
+    event.target.style.background ="blue";
+    event.target.style.color ="white";
+});
+
+const roar = document.getElementById("image5");
+
+roar.addEventListener("click", (event) =>{
+    event.target.style.background ="blue";
+    event.target.style.color ="white";
+});
+
+const row = document.getElementById("image6");
+
+row.addEventListener("click", (event) =>{
+    event.target.style.background ="blue";
+    event.target.style.color ="white";
+});
